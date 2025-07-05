40f45d6345d097d6be320da1c1459c9e164909c8
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-07-05 09:59:29 +0200

    consegna ex03 bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -4,28 +4,27 @@ const shoppingList = 'mele, banane, arance, uva'; // NON MODIFICARE QUESTA RIGA
 const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 
 // 1. Cerca un carattere in una stringa
-// Stampa l'INDICE con cui viene trovato
-// il carattere Y nella variabile city
-const cityIndex = 0;
+// Stampa l'INDICE con cui viene trovato il carattere Y nella variabile city
+const cityIndex = city.indexOf('Y');
+// L’indice di 'Y' in "New York" è 4 (partendo da 0)
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = '';
+const citySubstring = city.slice(4); // oppure: city.substring(4)
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
-// Concatena due stringhe usando la sintassi `${var}`
-// e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = '';
+// Concatena due stringhe usando la sintassi `${var}` e rendi cityNameConcat uguale a 'New York City'
+const cityNameConcat = `${city} ${cityName}`; // Concateno "New York" + "City"
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = '';
+const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
