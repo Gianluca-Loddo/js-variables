8534229255d55d47c495d687bc181b6768ab9d02
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-07-05 09:36:06 +0200

    consegna ex 02

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,10 +2,18 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+const name = "Gianluca";
+console.log(name); // Output: "Gianluca"
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+const number = 42; // Un numero
+const text = "Hello, World!"; // Una stringa
+const bool = true; // Un valore booleano
+console.log(number, text, bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +22,21 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+// Per concatenare le stringhe, si può utilizzare il template literal
+// const fullName = `${firstName} ${lastName}`; // Utilizza i templateliterals
+// oppure si potrebbe utilizzare l'operatore di concatenazione +
+// const fullName = firstName + ' ' + lastName; // Utilizza l'// operatore di concatenazione
+
+const fullName = `${firstName} ${lastName}`; // Concatenazione delle stringhe
 console.log(fullName); // Output: "Jane Smith"
+
+// per poter utilizzare l'operatore di concatenazione verrà dichiarata una nuova variabile fullName2 in quanto fullName è già stato definito
+// In JavaScript, non è consentito dichiarare due volte una variabile con lo stesso nome nello stesso ambito quando si usa let o const.
+
+const fullName2 = firstName + ' ' + lastName; // Concatenazione delle stringhe
+console.log(fullName2); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length; // Lunghezza della stringa alphabet
 console.log(numberOfLettersInAlphabet); // Output: 26
